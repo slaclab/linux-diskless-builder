86bce7ec2fca1ee9f2b0f1dc1152599d406ddd88
Eric Gumtow <egumtow@slac.stanford.edu>
2026-02-18 09:55:24 -0800

    use + one-or-more matching, and match PCI domains higher than 0

--- custom_files/disable_disconnected_nics.sh
+++ custom_files/disable_disconnected_nics.sh
@@ -11,14 +11,15 @@ should_down()
         fi
 
         # Check if the interface is bridged by USB (PCI -> USB -> ethernet)
-        if [[ "$1" =~ enp0s[0-9]{1,}f[0-9]u[0-9]{1,} ]] ; then
+        if [[ "$1" =~ enp[0-9]+s[0-9]+f[0-9]u[0-9]+ ]] ; then
             # Naming convention documentation: https://www.freedesktop.org/software/systemd/man/latest/systemd.net-naming-scheme.html
             # Table 3: slot naming schemes
-            #  en            --> ethernet
-            #  p0            --> PCI domain 0
-            #  s[0-9]{1,}    --> PCI slot 0 or higher, seems to be decimal from code (safe_atou*())
-            #  f[0-9]        --> PCI function 0 to 9
-            #  u[0-9]{1,}    --> USB port 0 or higher
+            #  en         --> ethernet
+            #  p[0-9]+    --> PCI domain 0 or higher
+            #  s[0-9]+    --> PCI slot 0 or higher
+            #  f[0-9]     --> PCI function 0 to 9
+            #  u[0-9]+    --> USB port 0 or higher
+            # all numbers seem to be in decimal from examining the code (safe_atou*()).
             #
             # Minh has "enp0s20f0u7u4", which matches and additionally is behind a second USB port (u7u4).
             return 1
